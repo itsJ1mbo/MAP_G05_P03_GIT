83353796f572d793cfe87a90c9d0d596e711ded7
Jimbo <javicentear@gmail.com>
2023-03-26 14:54:45 +0200

    no

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Cosas");
+
+            Console.WriteLine("Git borracho putero estafador");
+
+            Console.WriteLine("Por favor que esto genere un conflicto");
+
+            Console.WriteLine("aaaaa");
+
+            Console.WriteLine("Fino");
+
+            Console.WriteLine("Booooof menudo conflicto va a salir (espero)");
+
+            Console.WriteLine("Booooof menudo conflicto va a salir (espero parte 2)");
+        }
+    }
+}
