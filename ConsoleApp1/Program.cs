70617c9bcab367536a757af3cc03b7e8460a2fad
David-104 <118064911+David-104@users.noreply.github.com>
2023-03-26 14:17:00 +0200

    aaaa

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
 
-            Console.WriteLine("Conflcito");
+            Console.WriteLine("Git borracho putero estafador");
         }
     }
 }
