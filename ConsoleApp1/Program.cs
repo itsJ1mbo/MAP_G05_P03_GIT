5b697cf42db770d899e6657c1a9d1cb77ed3a4b3
Jimbo <javicentear@gmail.com>
2023-03-26 17:21:24 +0200

    checkout

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,38 +5,6 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-
-            Console.WriteLine("Cosas");
-
-            Console.WriteLine("Git borracho putero estafador");
-
-            Console.WriteLine("Puta");
-
-            Console.WriteLine("aaaaa");
-
-            Console.WriteLine("Fino");
-
-            Console.WriteLine("Booooof menudo conflicto va a salir (espero)");
-
-            Console.WriteLine("Booooof menudo conflicto va a salir (espero parte 2)");
-
-            Console.WriteLine("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-
-            Console.WriteLine("Yo mas");
-
-            Console.WriteLine("Continua el sufrimiento");
-
-            Console.WriteLine("Creemos que funciona");
-
-            Console.WriteLine("FUNCIONAAAAAAAAAAAAAAAAA");
-
-            Console.WriteLine("Patata");
-
-            Console.WriteLine("Por favor funciona :(");
-
-            Console.WriteLine("Volvemos a lo anterior por idiotas");
-
-            Console.WriteLine("No puedo mas");
         }
     }
 }
